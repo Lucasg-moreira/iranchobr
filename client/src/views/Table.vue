decb40ad9cb859235f421bc34c5f3c14fd403213
Lucasg-moreira <lucasmoreira.gabriel2004@gmail.com>
2022-09-18 16:39:40 -0300

    .

--- client/src/views/Table.vue
+++ client/src/views/Table.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
     <button @click="addPeoples()">
-      <router-link to="/pessoas">Add</router-link>
-    </button>
-    <h1>Tabela</h1>
-    <router-view />
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Email</th>
-          <th scope="col">Endereço</th>
-          <th scope="col">sexo</th>
-          <th scope="col">ic</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="pessoa in data">
-          <td>{{ pessoa.id }}</td>
-          <td>{{ pessoa.no_pessoa }}</td>
-          <td>{{ pessoa.no_email }}</td>
-          <td>{{ pessoa.endereco }}</td>
-          <td>{{ pessoa.sexo }}</td>
-          <td>{{ pessoa.ic_ativo }}</td>
-          <div>
-            <button>Details</button>
-            <button>Edit</button>
-            <button @click="remove(pessoa.id)">Remove</button>
-          </div>
-        </tr>
-      </tbody>
-    </table>
+        <router-link to="/pessoas">Add</router-link>
+      </button>
+    <div v-if="data.length > 0">
+      <h1>Tabela</h1>
+      <router-view />
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Endereço</th>
+            <th scope="col">sexo</th>
+            <th scope="col">ic</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pessoa in data">
+            <td>{{ pessoa.id }}</td>
+            <td>{{ pessoa.no_pessoa }}</td>
+            <td>{{ pessoa.no_email }}</td>
+            <td>{{ pessoa.endereco }}</td>
+            <td>{{ pessoa.sexo }}</td>
+            <td>{{ pessoa.ic_ativo }}</td>
+            <div>
+              <button>Details</button>
+              <button>Edit</button>
+              <button @click="remove(pessoa.id)">Remove</button>
+            </div>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <h1>Não tem nenhum item na tabela! Tente adicionar um!</h1>
+    </div>
   </div>
 </template>
 
